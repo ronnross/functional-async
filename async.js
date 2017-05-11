ce4b9d68994da02006e14c8c9844d77fe8ba2aaf
Ronn Ross <Ronn.ross@gmail.com>
2017-05-10 22:14:04 -0400

    adding pipe

--- async.js
+++ async.js
@@ -1,7 +1,8 @@
 const fetch = require("node-fetch")
 
 const toJson = r => r.json()
-
+const cp = method => (...fns) => initialValue => fns[method]((value, fn) => fn(value), initialValue);
+const pipe = cp('reduce')
 async function showGitHubUser(handle) {
   const response = await fetch(`https://api.github.com/users/${handle}`)
   return await response.json()
@@ -12,6 +13,12 @@ async function showGitHubUser2(handle) {
 }
 
 showGitHubUser("ronnross")
+  .then(user => {
+    console.log(user.name)
+    console.log(user.location)
+  })
+
+showGitHubUser2("ronnross")
   .then(user => {
     console.log(user.name)
     console.log(user.location)
